30b5714122d209a8faf160fd3fda2fd7086be50d
my name <boruchmichal@gmail.com>
2024-03-07 10:28:04 +0100

    test init class

--- python/test_vector.py
+++ python/test_vector.py
@@ -9,6 +9,21 @@ from .vector import Vector
 
 def test_init_vector():
     # arrange
-    v = Vector([1,2,3])
+    initial_list = [1, 2, 4.3]
+    expected_repr = "Vector([1.0, 2.0, 4.3])"
+    expected_value = initial_list
     # act
+    v = Vector(initial_list)
     # assert
+    assert str(v) == expected_repr
+    assert v.values == expected_value
+
+
+def test_init_vector__invalid_input():
+    # arrange
+    initial_list = [1, 2, "3"]
+    expected_error_msg = "Vector elements must be type float"
+    # act & assert
+    with pytest.raises(ValueError) as exc:
+        Vector(initial_list)
+    assert str(exc.value) == expected_error_msg
--- python/vector.py
+++ python/vector.py
@@ -9,7 +9,17 @@ class Vector:
     def __init__(self, initial_list: List[Real]):
         if not self._are_elements_valid(initial_list):
             raise ValueError("Vector elements must be type float")
+        self._vector = [float(i) for i in initial_list]
+
+    def __repr__(self):
+        class_name = type(self).__name__
+        return f"{class_name}({self._vector})"
 
     @staticmethod
     def _are_elements_valid(input_list: List) -> bool:
         return all([isinstance(i, Real) for i in input_list])
+
+    @property
+    def values(self):
+        return self._vector
+
